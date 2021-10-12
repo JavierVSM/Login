123c771293ad1e1aa083f58c6ac0529236fcc041
JavierVSM <javier.vsm@gmail.com>
2021-10-12 11:50:29 -0600

    Second commit

--- login_app/controllers/users_controller.py
+++ login_app/controllers/users_controller.py
@@ -1,77 +1,72 @@
 from flask import render_template, request, redirect, session, flash
+from flask_bcrypt import Bcrypt 
+from werkzeug import datastructures
 from login_app import app
 from login_app.models.user import User
-from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt(app)
 
+#Index page
 @app.route("/")
-def getStart():
+def index():
     return render_template("index.html")
 
-@app.route("/add_user", methods=['POST'])
-def add_new_user():
-    fName = request.form['firstname']
-    lName = request.form['lastname']
-    email = request.form['email']
-    password = request.form['password']
-    encrypted_password = bcrypt.generate_password_hash(password)
-    doble_password = request.form['doblepassword']
-
-
-    if User.validate_data(fName, lName, email, encrypted_password, password, doble_password):
-        nuser = User(fName, lName, email, encrypted_password)
-        User.addUser(nuser)
-        return redirect("/")
-    else:
-        print("something went wrong")
-        return redirect("/")
-
-#Para Login
-#@app.route("/login", methods=['POST'])
-#def displayLogin():
-#    loginError = ""
-#    if 'loginError' in session:
-#        loginError = session['loginError']
-#    return render_template("index.html", loginError=loginError)
-
-
-@app.route('/authentication', methods=['POST'])                           
-def validateCredentials():
-    email = request.form ['email']
-    password = request.form ['password']
-    result = User.validate_Email(email)
-    if result == ():
-        flash("Email not registered")
-        return redirect("/")
-    else:
-        verificated_password = result[0]['password']
-        if result[0]['email'] == email:
-            if bcrypt.check_password_hash(verificated_password, password):
-                session.clear()
-                data={
-                    'id':result[0]['id'],
-                    'fName': result[0]['fName']
-                    }
-                session['id'] = result[0]['id']
-                print(session['id'])
-                return redirect("/home")
-            else:
-                flash("Wrong credentials provided.")
-    return redirect ('/')
-
-@app.route("/home", methods=['GET'])
-def get_home():
+#home page
+@app.route("/home")
+def home():
     if 'id' not in session:
         return redirect('/')
-    user_info = User.start_session(session['id'])
-    data = {
-        "id": session['id'],
-        "fName": user_info[0]['fName']
-    }
-    return render_template("home.html", info=data)
+    return render_template("home.html")    
 
+#Add a new user - register.
+@app.route("/new_user", methods=["POST"])
+def addUser():
+    data = {
+        "fName": request.form["fName"],
+        "lName" : request.form["lName"],
+        "email" : request.form["email"],
+        "password" : request.form["password"],
+        "doblepassword" : request.form["doblepassword"],
+        "encrypted_password" : bcrypt.generate_password_hash(request.form['password'])
+    }
+    verify=User.user_at_db(data)
+    if (len(verify)) < 1:
+        if not User.validate_user(data):
+            return redirect('/')
+        User.add_user(data)
+        flash("Thank you. Please login with your information.")
+    else:
+        flash("Email already registered")
+    return redirect('/')
+
+#Login
+@app.route("/authentication", methods=['POST'])
+def validation():
+    email = request.form["email"]
+    password = request.form["password"]
+    verifyemail=User.validate_email(email)
+    if verifyemail == ():
+            flash("Email not registered")
+            return redirect("/")
+    else:
+        password_at_db = verifyemail[0]['password']
+        if verifyemail[0]['email'] == email:
+            if bcrypt.check_password_hash(password_at_db, password):
+                session.clear()
+                data={
+                    'id':verifyemail[0]['id'],
+                    'fName': verifyemail[0]['fName']
+                }
+                session['id'] = verifyemail[0]['id']
+                session['name'] = verifyemail[0]['fName']
+                print (session['id'], session['name'])
+                return redirect("/home")
+            else:
+                flash("Wrong password, try again")
+    return redirect('/')
+    
+#Logout
 @app.route("/logout", methods=['GET'])
 def logout_session():
     session.clear()
-    return redirect("/")
+    return redirect('/')
--- login_app/models/user.py
+++ login_app/models/user.py
@@ -1,91 +1,57 @@
-from flask import flash, session
+from werkzeug import datastructures
 from login_app.config.mysqlconnection import connectToMySQL
+from flask import flash, session
 import re
 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
 class User:
-    def __init__( self , email, fName, lName, password):
-        self.email = email
-        self.fName = fName
-        self.lName = lName
-        self.password = password
+    def __init__( self , data):
+        self.id = data['id']
+        self.email = data['email']
+        self.fName = data['fName']
+        self.lName = data['lName']
+        self.password = data['password']
+
+
+    #New user  
+    @classmethod
+    def add_user(cls, data):
+        query = "INSERT INTO users ( fName , lName , email , password ) VALUES ( %(fName)s , %(lName)s , %(email)s , %(encrypted_password)s );"
+        return connectToMySQL('login_db').query_db( query, data )
 
     @classmethod
-    def addUser(cls, nuser):
-        query = "INSERT INTO users(email, fName, lName, password) VALUES (%(email)s, %(fName)s , %(lName)s , %(password)s);"
-        data={
-            "email": nuser.email,
-            "fName": nuser.fName,
-            "lName": nuser.lName,
-            "password": nuser.password
-        }
-        result = connectToMySQL("login_db").query_db(query,data)
-        return result
+    def user_at_db(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        return connectToMySQL('login_db').query_db( query, data)
     
     @staticmethod
-    def validate_data(fName, lName, email, encrypted_password, password, doble_password):
-        isValid = True
-        
-        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-
-        query = "SELECT email FROM users WHERE email = %(email)s;"
-        email_data = {
-                "email" : email,
-            }
-        results = connectToMySQL("login_db").query_db(query,email_data)
-
-        print(len(results))
-
-        if not EMAIL_REGEX.match(email):
-            flash("Invalid email, please write email in valid format")
+    def validate_user(data):
+        is_valid = True
+        if len(data['fName']) < 2:
+            flash("First name must be at least 2 characters.")
+            is_valid = False
+        if len(data['lName']) < 2:
+            flash("Last name must be at least 2 characters.")
+            is_valid = False
+        if not EMAIL_REGEX.match(data['email']): 
+            flash("Invalid email address!")
+            is_valid = False 
+        if len(data['password']) < 8:
+            flash("Password must be at least 8 characters.")
+            is_valid = False    
+        if data['password'] != data['doblepassword']:
+            flash("Password confirmation does not match the first password, please try again")
             isValid = False
-
-        elif results != False and results != ():
-            flash("Email already registered")
-            isValid = False
-
-        if len( fName ) < 2:
-            flash( "First name must be at least 2 characters long" )
-            isValid = False 
-
-        if len( lName ) < 2:
-            flash( "Last name must be at least 2 characters long")
-            isValid = False
-
-        if len(password) < 8:
-            flash("Password must be at least 8 characters long")
-            isValid = False
-
-        if password != doble_password:
-            flash("Passwords must match, try again")
-            isValid = False
-        return isValid
-
-     #Para login:
+        return is_valid
+    
+    #Login:
     @classmethod
-    def get_user_to_validate(cls, email, password):
-        query = "SELECT * FROM users WHERE email=%(email)s AND password=%(password)s;"
-        data= {
-            "email":email,
-            "password":password
-        }
-        result = connectToMySQL('login_db').query_db(query, data)
-        return result
-
-    @classmethod
-    def validate_Email(cls, email):
+    def validate_email(cls, email):
         isValid = True
         query = "SELECT * FROM users WHERE email = %(email)s;"
         email={
             "email": email
-            }
+        }
         result = connectToMySQL("login_db").query_db(query,email)
         return result
-
-    @classmethod
-    def start_session(cls,data):
-        query = "SELECT fName FROM users WHERE id = %(id)s;"
-        data = {
-            "id": session['id'],
-        }
-        results = connectToMySQL("login_db").query_db(query,data)
-        return results
